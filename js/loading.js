8a964b12a96defa9c085581e5a22e90c7d371cd8
aoe3 <aoe3@cornell.edu>
2017-10-12 21:34:56 -0400

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,14 +1,14 @@
-// $(window).load(function() {
-//     $('#loadScreen').hide();
-//     $('#picMenu').show();
-// });
-
-//for demoing loader
 $(window).load(function() {
-    setTimeout('endLoader()', 2000);
+    $('#loadScreen').hide();
+    $('#picMenu').show();
 });
 
-function endLoader(){
-	$('#loadScreen').hide();
-	$('#picMenu').show();
-}
+//for demoing loader
+// $(window).load(function() {
+//     setTimeout('endLoader()', 2000);
+// });
+
+// function endLoader(){
+// 	$('#loadScreen').hide();
+// 	$('#picMenu').show();
+// }
